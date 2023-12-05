c7988c0af586579bc3f984e6b350c1d90bc30ebe
Jake Baker <SEP.Baker@fronius.com>
2023-12-05 10:49:09 -0500

    Day5: prolog, simplified.

--- prolog/day05/day5.pl
+++ prolog/day05/day5.pl
@@ -1,77 +1,37 @@
 :- [day5_parser].
 
-mapsequence([
-  'seed-to-soil',
-  'soil-to-fertilizer',
-  'fertilizer-to-water',
-  'water-to-light',
-  'light-to-temperature',
-  'temperature-to-humidity',
-  'humidity-to-location'
-]).
+subsequent('seed-to-soil', 'soil-to-fertilizer').
+subsequent('soil-to-fertilizer', 'fertilizer-to-water').
+subsequent('fertilizer-to-water', 'water-to-light').
+subsequent('water-to-light', 'light-to-temperature').
+subsequent('light-to-temperature', 'temperature-to-humidity').
+subsequent('temperature-to-humidity', 'humidity-to-location').
 
-do_map(Source, Kind, Destination) :-
-  map(Kind, DestinationRangeStart, SourceRangeStart, Length),
-  Source >= SourceRangeStart,
-  Source < SourceRangeStart + Length,
-  !,
-  Destination is Source - SourceRangeStart + DestinationRangeStart.
 
-do_map(_, Kind, _) :-
-  \+ once(map(Kind, _, _, _)),
-  throw(error(domain_error(kind, Kind), 'invalid kind')).
-
-do_map(Source, _, Source). 
-
--s2l([], Result, Result).
--s2l([Head|Tail], Accum, Result) :-
-  do_map(Accum, Head, Accum2),
-  -s2l(Tail, Accum2, Result).
-
-seed_to_location(Seed, Location) :-
-  mapsequence(Sequence),
-  -s2l(Sequence, Seed, Location).
+walk(SourceStart, SourceRange, Kind, Result) :-
+  map(Kind, DRS, SRS, RL),
+  % writeln([SourceStart, SourceRange, Kind, DRS, SRS, RL]),
+  SourceStart + SourceRange -1 >= SRS,
+  SourceStart < SRS + RL,
+  NewStart is max(SourceStart, SRS),
+  Offset is NewStart - SRS,
+  NewRL is min(SourceStart + SourceRange, SRS + RL) - NewStart,
+  DestinationStart is DRS + Offset,
+  (subsequent(Kind, Kind2) -> walk(DestinationStart, NewRL, Kind2, Result) ; Result=DestinationStart).
 
 do_part1(Result) :-
   findall(Location, (
     seed(X),
-    seed_to_location(X, Location)
+    walk(X, 1, 'seed-to-soil', Location)
   ), Locations),
   min_list(Locations, Result).
 
-% Rule is a list with values DestinationRangeStart, SourceRangeStart, RangeLength
-create_new_range_rule(OldDestinationRangeStart, OldSourceStart, SourceRange, Kind, Rules) :-
-  SourceStart is OldDestinationRangeStart,
-  findall([NewDRS, NewSRS, NewRL], (
-    map(Kind, DRS, SRS, RL),
-    SourceStart + SourceRange -1 >= SRS,
-    SourceStart < SRS + RL,
-    NewStart is max(SourceStart, SRS),
-    NewRL is min(SourceStart + SourceRange, SRS + RL) - NewStart,
-    NewSRS is OldSourceStart + NewStart - SourceStart,
-    NewDRS is  DRS + NewStart - SRS
-  ), Rules).
-
-create_new_range_rules(_, [], []).
-create_new_range_rules(Kind, [[ODRS,SS,SR]|Tail], Result) :-
-  create_new_range_rule(ODRS, SS, SR, Kind, Result2), !,
-  writeln([[ODRS,SS,SR], Kind, Result2]),
-  append(Result2, Result3, Result),
-  create_new_range_rules(Kind, Tail, Result3), !.
-
-create_all_range_rules(Result) :-
-  findall([X, X, Y], seed_pair(X, Y), StartingRangeRules),
-  mapsequence(Sequence),
-  foldl(create_new_range_rules, Sequence, StartingRangeRules, Result).
-
-% -6284386055 - wrong
 do_part2(Result) :-
-  create_all_range_rules(Range),
-  findall(X, (
-    member([A, _, _], Range),
-    X is A
-  ), Xs),
-  min_list(Xs, Result).
+  findall(Location, (
+    seed_pair(X, Y),
+    walk(X, Y, 'seed-to-soil', Location)
+  ), Locations),
+  min_list(Locations, Result).
 
 :- begin_tests(day4).
 
