f7126456bca4690035beaaf10beec44e8c50f57f
Jake Baker <SEP.Baker@fronius.com>
2023-12-08 10:56:23 -0500

    simplified lcm_list.

--- prolog/utils.pl
+++ prolog/utils.pl
@@ -4,9 +4,4 @@ acc_product([Head|Tail], Acc, Result) :- X is Acc * Head, acc_product(Tail, X, R
 
 product(L, Result) :- acc_product(L, 1, Result).
 
-lcm_list_accum([], Accum, Accum).
-lcm_list_accum([A|T], Accum, Result) :- 
-  X is lcm(A,Accum),
-  lcm_list_accum(T, X, Result).
-
-lcm_list(L, Result) :- lcm_list_accum(L, 1, Result).
+lcm_list(L, Result) :- foldl([A,B,C]>>(C is lcm(A,B)), L, 1, Result).
