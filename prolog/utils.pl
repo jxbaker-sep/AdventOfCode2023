d4ce42a5a8123ab5289067d7a64ccfe769b72a5e
Jake Baker <SEP.Baker@fronius.com>
2023-12-08 10:58:31 -0500

    Simplified product.

--- prolog/utils.pl
+++ prolog/utils.pl
@@ -2,6 +2,6 @@
 acc_product([], Acc, Acc).
 acc_product([Head|Tail], Acc, Result) :- X is Acc * Head, acc_product(Tail, X, Result).
 
-product(L, Result) :- acc_product(L, 1, Result).
+product(L, Result) :- foldl([A,B,C]>>(C is A * B), L, 1, Result).
 
 lcm_list(L, Result) :- foldl([A,B,C]>>(C is lcm(A,B)), L, 1, Result).
