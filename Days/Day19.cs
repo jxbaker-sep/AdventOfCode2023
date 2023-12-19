49dce87e004d90062c01459d2a1c920c6f5f9dba
Jake Baker <SEP.Baker@fronius.com>
2023-12-19 12:01:27 -0500

    Day19

--- Days/Day19.cs
+++ Days/Day19.cs
@@ -73,21 +73,67 @@ public class Day19 : AdventOfCode<long, Day19Data>
 
 
     [TestCase(Input.Sample, 167409079868000)]
-    [TestCase(Input.Data, 0)]
+    [TestCase(Input.Data, 116606738659695)]
     public override long Part2(Day19Data world)
     {
-      var tps = new List<TheoreticalPart>();
-      tps = MatchTheoreticalPart(world.Workflows, "in", new TheoreticalPart(new Part(1,1,1,1), new Part(4000, 4000, 4000, 4000))).ToList();
-        
-      var oldTps = new List<TheoreticalPart>();
-      while(tps.Count != oldTps.Count)
+      var tps = new HashSet<TheoreticalPart>();
+      tps = MatchTheoreticalPart(world.Workflows, "in", new TheoreticalPart(new Part(1,1,1,1), new Part(4000, 4000, 4000, 4000))).ToHashSet();
+
+      var workflows = tps.Pairs()
+        .Select(p => Overlap(p.First, p.Second))
+        .OfType<TheoreticalPart>()
+        .Select(CreateExclusionWorkflow)
+        .ToList();
+
+      foreach(var workflow in workflows)
       {
-        oldTps = tps;
-        tps = tps.Pairs().SelectMany(it => SplitOverlaps(it.First, it.Second)).ToList();
+        tps = tps.SelectMany(tp => MatchTheoreticalPart(workflow, "z1", tp)).ToHashSet();
       }
 
-
       return tps.Sum(it => (it.Max.A - it.Min.A + 1)*(it.Max.X - it.Min.X + 1)*(it.Max.M - it.Min.M + 1)*(it.Max.S - it.Min.S + 1));
+    }
+
+    public Workflows CreateExclusionWorkflow(TheoreticalPart p)
+    {
+      var rules = new List<Rule>
+      {
+          new(new('x', '<', p.Min.X), "A"),
+          new(new('x', '>', p.Max.X), "A"),
+          new(new('m', '<', p.Min.M), "A"),
+          new(new('m', '>', p.Max.M), "A"),
+          new(new('a', '<', p.Min.A), "A"),
+          new(new('a', '>', p.Max.A), "A"),
+          new(new('s', '<', p.Min.S), "A"),
+          new(new('s', '>', p.Max.S), "A"),
+          new(null, "R")
+      };
+
+      var wf = new Workflow("z1", rules);
+      return new Dictionary<string, Workflow>{{wf.Name, wf}};
+    }
+
+    public TheoreticalPart? Overlap(TheoreticalPart p1, TheoreticalPart p2)
+    {
+      var result = new TheoreticalPart(
+        new Part(
+          Math.Max(p1.Min.X, p2.Min.X),
+          Math.Max(p1.Min.M, p2.Min.M),
+          Math.Max(p1.Min.A, p2.Min.A),
+          Math.Max(p1.Min.S, p2.Min.S)
+        ),
+        new Part(
+          Math.Min(p1.Max.X, p2.Max.X),
+          Math.Min(p1.Max.M, p2.Max.M),
+          Math.Min(p1.Max.A, p2.Max.A),
+          Math.Min(p1.Max.S, p2.Max.S)
+        )
+      );
+      if (result.Min.X <= result.Max.X &&
+          result.Min.M <= result.Max.M &&
+          result.Min.A <= result.Max.A &&
+          result.Min.S <= result.Max.S)
+        return result;
+      return null;
     }
 
     IEnumerable<TheoreticalPart> SplitOverlaps(TheoreticalPart p1, TheoreticalPart p2)
@@ -127,6 +173,15 @@ public class Day19 : AdventOfCode<long, Day19Data>
         var x = minRight;
         var y = new[]{maxRight, maxLeft}.Min();
         var otherFields = new[]{'a', 'x', 'm', 's'}.Where(it => it != field).ToList();
+        var result = new TheoreticalPart(new Part(1,1,1,1), new Part(4000,4000,4000,4000));
+        result = SetMin(result, field, x);
+        result = SetMax(result, field, y);
+        foreach(var f in otherFields)
+        {
+          result = SetMin(result, f, new[]{GetMin(left, f), GetMin(right, f)}.Min());
+          result = SetMax(result, f, new[]{GetMax(left, f), GetMax(right, f)}.Min());
+        }
+        yield return result;
       }
 
       if (splitCount == 0)
@@ -145,7 +200,24 @@ public class Day19 : AdventOfCode<long, Day19Data>
       {
         var results = NarrowAndApply(workflows, rule, part);
         foreach(var result in results) yield return result;
+        part = ReverseRule(rule.Condition, part);
       }
+    }
+
+    public TheoreticalPart ReverseRule(Condition? c, TheoreticalPart part)
+    {
+      if (c == null) return part; // shouldnt matter because non-conditional rule is last
+      if (c.Operator == '<')
+      {
+        var newRule = new Condition(c.Field, '>', c.Value - 1);
+        return Narrow(newRule, part) ?? throw new ApplicationException();
+      }
+      else if (c.Operator == '>')
+      {
+        var newRule = new Condition(c.Field, '<', c.Value + 1);
+        return Narrow(newRule, part) ?? throw new ApplicationException();
+      }
+      throw new ApplicationException();
     }
 
     public IEnumerable<TheoreticalPart> NarrowAndApply(Workflows workflows, Rule rule, TheoreticalPart part)
@@ -154,7 +226,7 @@ public class Day19 : AdventOfCode<long, Day19Data>
       var p = Narrow(c, part);
       if (p != null)
       {
-        if (rule.Destination == "A") yield return part;
+        if (rule.Destination == "A") yield return p;
         else if (rule.Destination == "R") yield break;
         else foreach(var sub in MatchTheoreticalPart(workflows, rule.Destination, p)) yield return sub;
       }
@@ -168,14 +240,14 @@ public class Day19 : AdventOfCode<long, Day19Data>
         if (c.Operator == '<')
         {
           if (min >= c.Value) return null;
-          max = new[]{max, c.Value-1}.Max();
+          max = new[]{max, c.Value-1}.Min();
           if (min > max) return null;
           return SetMax(part, c.Field, max);
         }
         else if (c.Operator == '>')
         {
           if (max <= c.Value) return null;
-          min = new[]{min, c.Value+1}.Min();
+          min = new[]{min, c.Value+1}.Max();
           if (min > max) return null;
           return SetMin(part, c.Field, min);
         }
