5ce898f3cb2c3da43ac6180a4bb0382acab7d4a8
Jake Baker <SEP.Baker@fronius.com>
2023-12-19 12:23:15 -0500

    stuff

--- Days/Day19.cs
+++ Days/Day19.cs
@@ -76,119 +76,9 @@ public class Day19 : AdventOfCode<long, Day19Data>
     [TestCase(Input.Data, 116606738659695)]
     public override long Part2(Day19Data world)
     {
-      var tps = new HashSet<TheoreticalPart>();
-      tps = MatchTheoreticalPart(world.Workflows, "in", new TheoreticalPart(new Part(1,1,1,1), new Part(4000, 4000, 4000, 4000))).ToHashSet();
-
-      var workflows = tps.Pairs()
-        .Select(p => Overlap(p.First, p.Second))
-        .OfType<TheoreticalPart>()
-        .Select(CreateExclusionWorkflow)
-        .ToList();
-
-      foreach(var workflow in workflows)
-      {
-        tps = tps.SelectMany(tp => MatchTheoreticalPart(workflow, "z1", tp)).ToHashSet();
-      }
+      var tps = MatchTheoreticalPart(world.Workflows, "in", new TheoreticalPart(new Part(1,1,1,1), new Part(4000, 4000, 4000, 4000))).ToList();
 
       return tps.Sum(it => (it.Max.A - it.Min.A + 1)*(it.Max.X - it.Min.X + 1)*(it.Max.M - it.Min.M + 1)*(it.Max.S - it.Min.S + 1));
-    }
-
-    public Workflows CreateExclusionWorkflow(TheoreticalPart p)
-    {
-      var rules = new List<Rule>
-      {
-          new(new('x', '<', p.Min.X), "A"),
-          new(new('x', '>', p.Max.X), "A"),
-          new(new('m', '<', p.Min.M), "A"),
-          new(new('m', '>', p.Max.M), "A"),
-          new(new('a', '<', p.Min.A), "A"),
-          new(new('a', '>', p.Max.A), "A"),
-          new(new('s', '<', p.Min.S), "A"),
-          new(new('s', '>', p.Max.S), "A"),
-          new(null, "R")
-      };
-
-      var wf = new Workflow("z1", rules);
-      return new Dictionary<string, Workflow>{{wf.Name, wf}};
-    }
-
-    public TheoreticalPart? Overlap(TheoreticalPart p1, TheoreticalPart p2)
-    {
-      var result = new TheoreticalPart(
-        new Part(
-          Math.Max(p1.Min.X, p2.Min.X),
-          Math.Max(p1.Min.M, p2.Min.M),
-          Math.Max(p1.Min.A, p2.Min.A),
-          Math.Max(p1.Min.S, p2.Min.S)
-        ),
-        new Part(
-          Math.Min(p1.Max.X, p2.Max.X),
-          Math.Min(p1.Max.M, p2.Max.M),
-          Math.Min(p1.Max.A, p2.Max.A),
-          Math.Min(p1.Max.S, p2.Max.S)
-        )
-      );
-      if (result.Min.X <= result.Max.X &&
-          result.Min.M <= result.Max.M &&
-          result.Min.A <= result.Max.A &&
-          result.Min.S <= result.Max.S)
-        return result;
-      return null;
-    }
-
-    IEnumerable<TheoreticalPart> SplitOverlaps(TheoreticalPart p1, TheoreticalPart p2)
-    {
-      var splitCount = 0;
-      foreach(var field in new[]{'a', 'x', 'm', 's'})
-      {
-        var (left, right) = GetMin(p1, field) < GetMin(p1, field) ? (p1, p2) : (p2, p1);
-
-        var minLeft = GetMin(left, field);
-        var minRight = GetMin(right, field);
-        var maxLeft = GetMax(left, field);
-        var maxRight = GetMax(right, field);
-
-        if (maxLeft < minRight)
-        {
-          continue;
-        }
-        splitCount += 1;
-
-        if (minLeft < minRight) // area of left before right
-        {
-          yield return SetMax(left, field, minRight-1);
-        }
-
-        if (maxLeft > maxRight) // area of left after right
-        {
-          yield return SetMin(left, field, maxRight +1);
-        }
-
-        if (maxRight > maxLeft) // area of right after left
-        {
-          yield return SetMin(right, field, maxLeft +1);
-        }
-
-        // area of overlap
-        var x = minRight;
-        var y = new[]{maxRight, maxLeft}.Min();
-        var otherFields = new[]{'a', 'x', 'm', 's'}.Where(it => it != field).ToList();
-        var result = new TheoreticalPart(new Part(1,1,1,1), new Part(4000,4000,4000,4000));
-        result = SetMin(result, field, x);
-        result = SetMax(result, field, y);
-        foreach(var f in otherFields)
-        {
-          result = SetMin(result, f, new[]{GetMin(left, f), GetMin(right, f)}.Min());
-          result = SetMax(result, f, new[]{GetMax(left, f), GetMax(right, f)}.Min());
-        }
-        yield return result;
-      }
-
-      if (splitCount == 0)
-      {
-        yield return p1;
-        yield return p2;
-      }
     }
 
     public record TheoreticalPart(Part Min, Part Max);
@@ -206,7 +96,7 @@ public class Day19 : AdventOfCode<long, Day19Data>
 
     public TheoreticalPart ReverseRule(Condition? c, TheoreticalPart part)
     {
-      if (c == null) return part; // shouldnt matter because non-conditional rule is last
+      if (c == null) return part; // shouldnt matter since conditional-less rule is always last
       if (c.Operator == '<')
       {
         var newRule = new Condition(c.Field, '>', c.Value - 1);
